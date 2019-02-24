7200b77608ccf311332a64fae4b37919aef96ed1
Lokesh Reddy Enamala <lokeshreddyenamala@Lokeshs-MacBook-Pro.local>
2019-02-23 22:27:59 -0500

    server.js update

--- server.js
+++ server.js
@@ -6,13 +6,6 @@ app.set('view enginer','hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(__dirname + '/public'));
 
-app.use((req,res,next)=>{
-    next();
-})
-
-app.use((req,res,next) => {
-    req.render('maintainence');
-})
 
 app.get('/',(req,res) => {
     res.send('Welcome Lokesh!');
